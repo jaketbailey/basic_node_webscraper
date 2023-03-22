bacf83da34c81beb3e6c8b424918b9b95ac561a3
Jake Bailey <jake@jaketbailey.co.uk>
2023-03-22 09:06:00 +0000

    Added support for other DSpace university research respositories

--- index.js
+++ index.js
@@ -3,69 +3,74 @@ const fs = require('fs');
 const keywordsJSON = JSON.parse(fs.readFileSync('./keywords.json', 'utf8'));
 
 const init = async () => {
-    const getLink = (keyword, page, resultsPerPage) => { // Generates the PEARL link based on query (keyword) and page number
+    const getLink = (URL, keyword, page, resultsPerPage) => { // Generates the PEARL link based on query (keyword) and page number
         const query = keyword.replace(/\s/g, '+')
-        return(`https://pearl.plymouth.ac.uk/handle/10026.1/10912/discover?rpp=${resultsPerPage}&etal=0&query=${query}&group_by=none&page=${page}`);
+        return(`${URL}/discover?rpp=${resultsPerPage}&etal=0&query=${query}&group_by=none&page=${page}`);
     }
 
-    let csvList = []; // The array used to store all lines of the CSV file to be created
-    for (const keyword of keywordsJSON.keywords) {
-        console.log(`Currently scraping keyword: ${keyword}`)
-        for (let i = 0; i <= 1; i++){ //Put up to 1 pages for now, can increase if you wish.
-            const link = getLink(keyword, i, 100)
-            const response = await fetch(link)
-            const body = await response.text();
-            const $ = cheerio.load(body);
+    for (const URL of keywordsJSON.URLs) {
+        let csvList = []; // The array used to store all lines of the CSV file to be created
+        const universityName = URL.split('.')[1];
+        for (const keyword of keywordsJSON.keywords) {
+            console.log(`Currently scraping keyword: ${keyword}, University: ${universityName}`)
+            for (let i = 0; i <= 1; i++){ //Put up to 1 pages for now, can increase if you wish.
+                const link = getLink(URL, keyword, i, 100)
+                const response = await fetch(link)
+                const body = await response.text();
+                const $ = cheerio.load(body);
 
-            $('.artifact-description').map((i, elem) => { // Looks for Div with class artifact-description which holds the key info to scrape
-                const titleNode = $(elem).find('a')
-                const infoNode = $(elem).find('.artifact-info')
+                $('.artifact-description').map((i, elem) => { // Looks for Div with class artifact-description which holds the key info to scrape
+                    const titleNode = $(elem).find('a')
+                    const infoNode = $(elem).find('.artifact-info')
 
-                const artifact = { // Object representing the current artifact/research
-                    title: '',
-                    href: '',
-                    author: '',
-                    date: '',
-                    abstract: '',
-                    keyword,
-                };
+                    const artifact = { // Object representing the current artifact/research
+                        title: '',
+                        href: '',
+                        author: '',
+                        date: '',
+                        abstract: '',
+                        keyword,
+                    };
 
-                // Below assigns all the relevant content from the Div to artifact object
-                artifact.title = (titleNode.text())
-                artifact.title = (artifact.title).replace(/\r?\n|\r/g, " "); //remove new line
-                artifact.href = `https://pearl.plymouth.ac.uk/${titleNode.attr('href')}`
-                artifact.author =  infoNode.find('.author').text();
-                artifact.date = infoNode.find('.publisher-date').text();
-                artifact.abstract = infoNode.find('.abstract').text();
-                artifact.abstract = (artifact.abstract).replace(/\r?\n|\r/g, " "); //remove new line
+                    // Below assigns all the relevant content from the Div to artifact object
+                    artifact.title = (titleNode.text())
+                    artifact.title = (artifact.title).replace(/\r?\n|\r/g, " "); //remove new line
+                    artifact.href = titleNode.attr('href')
+                    artifact.author =  infoNode.find('.author').text();
+                    artifact.date = infoNode.find('.publisher-date').text();
+                    artifact.abstract = infoNode.find('.abstract').text();
+                    artifact.abstract = (artifact.abstract).replace(/\r?\n|\r/g, " "); //remove new line
 
-                let csvString = '';
+                    let csvString = '';
 
-                for (const [key, value] of Object.entries(artifact)) { // Loops throught the artifact object and generates a CSV string using '_' delimiter
-                    if (key === 'title') {
-                        csvString += value;
-                    } else {
-                        csvString += `_${value}`;
+                    for (const [key, value] of Object.entries(artifact)) { // Loops throught the artifact object and generates a CSV string using '_' delimiter
+                        if (key === 'title') {
+                            csvString += value;
+                        } else {
+                            csvString += `_${value}`;
+                        }
                     }
-                }
 
-                csvList.push(csvString) // Pushes csvString to the array of csvStrings (csvList)
+                    csvList.push(csvString) // Pushes csvString to the array of csvStrings (csvList)
+                });
+            };
+
+            csvList = csvList.filter(function(elem, pos) { // Removes potential duplicates where the same artifact showed on multiple pages
+                return csvList.indexOf(elem) == pos;
             });
-        };
+        }
 
-        csvList = csvList.filter(function(elem, pos) { // Removes potential duplicates where the same artifact showed on multiple pages
-            return csvList.indexOf(elem) == pos;
-        });
+        console.log(URL.split('.')[1])
+        const file = fs.createWriteStream(`${universityName}.csv`); // Creates the file to store the scraped research info
+        file.on('error', function(err) { throw err });
+        file.write('sep=_\n'); // Specifies '_' delimiter
+        file.write('Title_Href_Author_Date_Abstract_Keyword\n'); // Adds Titles to each row in the CSV
+        for (const item of csvList) { // Writes the array to the CSV
+            file.write(`${item}\n`);
+        }
+        file.end();
     }
 
-    const file = fs.createWriteStream('plymouth.csv'); // Creates the file to store the scraped research info
-    file.on('error', function(err) { throw err });
-    file.write('sep=_\n'); // Specifies '_' delimiter
-    file.write('Title_Href_Author_Date_Abstract_Keyword\n'); // Adds Titles to each row in the CSV
-    for (const item of csvList) { // Writes the array to the CSV
-        file.write(`${item}\n`);
-    }
-    file.end();
 }
 
 init();
